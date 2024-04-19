214b95936d9c11fe9928a5d6fc22855c0248239d
ImGreatest <ImGreatest@users.noreply.github.com>
2024-04-19 03:10:22 +0300

    was added logger

--- libs/adapter/user/user.adapter.ts
+++ libs/adapter/user/user.adapter.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { UserRepository } from "../../domains/user/repositories/user.repository";
 import { IReqCreateUser } from "../../domains/user/dto/req-dto/req-create-user.interface.dto";
 import { IResUser } from "../../domains/user/dto/res-dto/res-user.dto";
@@ -14,17 +14,24 @@ export class UserAdapter extends UserRepository {
 	}
 
 	async createUser(user: IReqCreateUser): Promise<IResUser> {
+		Logger.verbose('createUser', user);
+
 		return this.prisma.user.create({
-      data: user,
-    });
+			data: user,
+		});
 	}
 
 	async getUserAndCount(): Promise<IResGetUserAndCountDto> {
+		Logger.verbose('getUserAndCount');
 		const users: User[] = await this.prisma.user.findMany();
     const totalCount: number = await this.prisma.user.count();
 
+		Logger.verbose('getUserAndCount', users, totalCount);
+
     if (!users)
       throw new Error();
+
+		Logger.verbose('getUserAndCount', users, totalCount);
 
     return {
       rows: users,
@@ -33,39 +40,57 @@ export class UserAdapter extends UserRepository {
 	}
 
 	async getUserByEmail(email: string): Promise<IResUser> {
+		Logger.verbose('getUserByEmail', email);
+
 		const user = this.prisma.user.findFirst({
       where: { email: email }
     });
 
+		Logger.verbose('getUserByEmail', user);
+
     if (!user)
       throw new Error();
+
+		Logger.verbose('getUserByEmail', user);
 
     return user;
 	}
 
 	async getUserById(id: number): Promise<IResUser> {
+		Logger.verbose('getUserById', id);
 		const user = this.prisma.user.findFirst({
       where: { id: id }
     });
 
+		Logger.verbose('getUserById', user);
+
     if (!user)
       throw new Error();
+
+		Logger.verbose('getUserById', user);
 
     return user;
 	}
 
 	async getUsers(): Promise<IResUser[]> {
+		Logger.verbose('getUsers');
 		const users = this.prisma.user.findMany({
       where: { deletedAt: null }
     });
 
+		Logger.verbose('getUserById', users);
+
     if (!users)
       throw new Error();
+
+		Logger.verbose('getUserById', users);
 
     return users;
 	}
 
 	async updateUser(id: number, user: IReqUpdateUser): Promise<IResUser> {
+		Logger.verbose('updateUser', id, user);
+
 		return this.prisma.user.update({
       where: {id: id},
       data: user,
@@ -73,6 +98,8 @@ export class UserAdapter extends UserRepository {
 	}
 
 	async deleteUser(id: number): Promise<IResUser> {
+		Logger.verbose('deleteUser', id);
+
 		return this.prisma.user.update({
       where: { id },
       data: { deletedAt: new Date() }
