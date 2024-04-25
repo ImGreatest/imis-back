c03d424ab16c587f5f0d50eb41a756967602bb2d
ImGreatest <cmayll@mail.ru>
2024-04-25 11:51:25 +0300

    Fix: adapters project.adapter.ts project.mock-adapter.ts; adds loggers

--- libs/adapter/project/project.adapter.ts
+++ libs/adapter/project/project.adapter.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ProjectRepository } from 'libs/domains/project/repositories/project.repository';
-import { IReqGetProject } from "libs/domains/project/dto/req-get-project.dto";
-import { IReqCreateProjectDto } from "libs/domains/project/dto/req-create-project.dto";
-import { Project } from "libs/entity/project";
-import { User } from "libs/entity/user";
-import { IReqUpdateProjectDto } from "libs/domains/project/dto/req-update-project.dto";
+import { Project } from 'libs/entity/project';
+import { IReqCreateProjectDto } from 'libs/domains/project/dto/req-create-project.dto';
+import { IReqGetProject } from 'libs/domains/project/dto/req-get-project.dto';
+import { User } from 'libs/entity/user';
+import { IReqUpdateProjectDto } from 'libs/domains/project/dto/req-update-project.dto';
 
 @Injectable()
 export class ProjectAdapter extends ProjectRepository {
@@ -13,26 +13,38 @@ export class ProjectAdapter extends ProjectRepository {
   }
 
   async createProject(data: IReqCreateProjectDto): Promise<Project> {
+    Logger.verbose(data);
+
     return Promise.resolve(undefined);
   }
 
   async getProjects(): Promise<IReqGetProject> {
-    return undefined
+    Logger.verbose('getProjects');
+
+    return undefined;
   }
 
   async getProjectByUser(user: User): Promise<IReqGetProject> {
+    Logger.verbose(user);
+
     return Promise.resolve(undefined);
   }
 
   async getProjectById(id: number): Promise<IReqGetProject> {
+    Logger.verbose(id);
+
     return Promise.resolve(undefined);
   }
 
   async updateProject(data: IReqUpdateProjectDto): Promise<Project> {
+    Logger.verbose(data);
+
     return Promise.resolve(undefined);
   }
 
   async deleteProject(projectId: number): Promise<void> {
+    Logger.verbose(projectId);
+
     return Promise.resolve(undefined);
   }
 }
--- libs/domains/project/repositories/project.repository.ts
+++ libs/domains/project/repositories/project.repository.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { IReqCreateProjectDto } from '../dto/req-create-project.dto';
 import { IReqUpdateProjectDto } from '../dto/req-update-project.dto';
 import { IReqGetProject } from '../dto/req-get-project.dto';
-import { Project } from "libs/entity/project";
-import { User } from "libs/entity/user";
+import { Project } from 'libs/entity/project';
+import { User } from 'libs/entity/user';
 
 @Injectable()
 export abstract class ProjectRepository {
