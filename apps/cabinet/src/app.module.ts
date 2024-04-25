d9db598df4e03237fb7b5e4fda3c8556a92b83ba
270rq <tulupov.valeriy@mail.ru>
2024-04-25 18:47:23 +0300

    fixed root

--- apps/cabinet/src/app.module.ts
+++ apps/cabinet/src/app.module.ts
@@ -2,12 +2,25 @@ import { Module } from '@nestjs/common';
 import { UserControllerModule } from './controllers/user/user-controller.module';
 import { PrismaModule } from 'libs/services/prisma/prisma.module';
 import { ProjectController } from './project/project.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from 'libs/services/auth/auth.guard';
+import { RolesGuard } from 'libs/services/auth/roles.guard';
 
 @Module({
   imports: [
     PrismaModule,
     // UserModule,
     UserControllerModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
   ],
   controllers: [ProjectController],
 })
