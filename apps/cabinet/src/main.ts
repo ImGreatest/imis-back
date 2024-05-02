9c1e3f49feda81f638692dc3f7b235df155cad50
ImGreatest <cmayll@mail.ru>
2024-05-02 14:21:54 +0300

    clear

--- apps/cabinet/src/main.ts
+++ apps/cabinet/src/main.ts
@@ -2,13 +2,9 @@ import { swagger } from './swagger';
 import { config } from 'config/config';
 import { AppModule } from './app.module';
 import { createNest } from 'libs/utils/create.nest';
-import { NestFactory } from "@nestjs/core";
 
 async function bootstrap() {
   await createNest(AppModule, 'users', swagger, +config.PortCabinet);
-  // const app = await NestFactory.create(AppModule);
-  // app.enableCors();
-  // await app.listen(3000);
 }
 
 void bootstrap();
