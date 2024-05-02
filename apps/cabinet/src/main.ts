b888c253414ae407bd21e02a08b45e70f902f3db
ImGreatest <cmayll@mail.ru>
2024-05-02 14:20:44 +0300

    fix-front

--- apps/cabinet/src/main.ts
+++ apps/cabinet/src/main.ts
@@ -1,10 +1,14 @@
 import { swagger } from './swagger';
-import { config } from '../../../config/config';
+import { config } from 'config/config';
 import { AppModule } from './app.module';
 import { createNest } from 'libs/utils/create.nest';
+import { NestFactory } from "@nestjs/core";
 
 async function bootstrap() {
-  createNest(AppModule, 'users', swagger, +config.PortCabinet);
+  await createNest(AppModule, 'users', swagger, +config.PortCabinet);
+  // const app = await NestFactory.create(AppModule);
+  // app.enableCors();
+  // await app.listen(3000);
 }
 
 void bootstrap();
