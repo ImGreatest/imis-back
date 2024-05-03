a24ade171000252bfb87d6e76992cc04dab51364
ImGreatest <cmayll@mail.ru>
2024-05-03 12:52:20 +0300

    email crud

--- apps/cabinet/src/controllers/email/email-controller.service.ts
+++ apps/cabinet/src/controllers/email/email-controller.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import { EmailService } from "libs/services/email/email.service";
+import { IMessageHtmlDto, IMessageTextDto } from "apps/cabinet/src/controllers/email/dto/message.dto";
+import { IConfirmDto } from "apps/cabinet/src/controllers/email/dto/confirm.dto";
 
 @Injectable()
 export class EmailControllerService {
 	constructor(private readonly emailService: EmailService) {}
 
-	sentMessage(to: string) {
-		return this.emailService.sendMail(to);
+	sentMessage(message: IMessageTextDto): Promise<void> {
+		return this.emailService.sendMessage(message);
+	}
+
+	sentConfirmActionMessage(message: IMessageHtmlDto): Promise<IConfirmDto> {
+		return this.emailService.confirmAction(message);
 	}
 }
--- apps/cabinet/src/controllers/email/email.controller.ts
+++ apps/cabinet/src/controllers/email/email.controller.ts
@@ -1,7 +1,9 @@
 import { ApiTags } from "@nestjs/swagger";
-import { Controller, Get, Post, Param, Body } from "@nestjs/common";
+import { Controller, Post, Body, Logger } from "@nestjs/common";
 import { EmailControllerService } from "apps/cabinet/src/controllers/email/email-controller.service";
 import { Public } from 'libs/decorators/public.decorator';
+import { IMessageHtmlDto, IMessageTextDto } from "apps/cabinet/src/controllers/email/dto/message.dto";
+import { IConfirmDto } from "apps/cabinet/src/controllers/email/dto/confirm.dto";
 
 @ApiTags('email-service')
 @Controller('email-service')
@@ -9,8 +11,15 @@ export class EmailController {
 	constructor(private readonly emailService: EmailControllerService) {}
 
 	@Public()
-	@Get('sent-message')
-	sentMail(@Param('to') to: string) {
-		return this.emailService.sentMessage(to);
+	@Post('sent-text-message')
+	sentMessage(@Body() message: IMessageTextDto): Promise<void> {
+		Logger.verbose("sentMessage", message);
+		return this.emailService.sentMessage(message);
+	}
+
+	@Public()
+	@Post('sent-confirm-message')
+	sentConfirmActionMessage(@Body() message: IMessageHtmlDto): Promise<IConfirmDto> {
+		return this.emailService.sentConfirmActionMessage(message);
 	}
 }
