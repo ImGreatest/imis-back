966418520b4a8a2753edb4be002b7a3637e0ed9b
ImGreatest <cmayll@mail.ru>
2024-05-03 12:52:20 +0300

    email crud

--- libs/services/email/email.service.ts
+++ libs/services/email/email.service.ts
@@ -1,41 +1,50 @@
 import { Injectable, Logger } from "@nestjs/common";
 import * as nodemailer from 'nodemailer';
 import SMTPTransport from "nodemailer/lib/smtp-transport";
-import { transport } from "winston";
+import { emailConfig } from "config/config";
+import { IMessageHtml, IMessageText } from "libs/services/email/interfaces/message.interface";
+import { IConfirm } from "libs/services/email/interfaces/confirm.interface";
 
 @Injectable()
 export class EmailService {
 	private transporter: nodemailer.Transporter<SMTPTransport.SentMessageInfo>;
 
 	constructor() {
+		Logger.verbose(emailConfig.EmailFrom, emailConfig.EmailPass);
+
 		this.transporter = nodemailer.createTransport({
-			host: 'smtp.ethereal.email',
-			port: 587,
-			secure: false, // true for 465
-			auth: {
-				user: 'ardella45@ethereal.email',
-				pass: 'KSbTKwtVZppk6nZkQE'
+				// mail.ru
+				host: 'smtp.mail.ru',
+				port: 465,
+				secure: true, // true for 465, false for other ports
+				auth: {
+					user: emailConfig.EmailFrom,
+					pass: emailConfig.EmailPass,
+				}
+			},
+				{
+					name: 'Экосистема IMIS',
+					from: 'Экосистема IMIS <imis_letter_sendler@mail.ru>',
 			}
-		},
-			{
-			from: 'mailer test <ardella45@ethereal.email>',
-		}
 		);
 	}
 
-	async sendMail(to: string) {
-		console.log(to);
-		Logger.verbose("sendMail", to);
-
-		const message = {
-			to: 'ardella45@ethereal.email',
-			subject: 'COngratilation!!!',
-			text: "ПОздравляю нахуй"
-		}
-
-		this.transporter.sendMail(message, (err, info) => {
-			if (err) return console.log(err);
-			console.log("email sent", info);
+	async sendMessage(message: IMessageText) {
+		console.log(message.to, message.subject, message.text);
+		this.transporter.sendMail(message, (err: Error) => {
+			if (err) return Logger.verbose(err);
 		})
 	}
+
+	async confirmAction(message: IMessageHtml): Promise<IConfirm> {
+		Logger.verbose("confirmAction", message);
+
+		this.transporter.sendMail(message, (err: Error) => {
+			if (err) return Logger.verbose(err);
+		});
+
+		return {
+			access: Math.floor(100000 + Math.random() * 900000)
+		}
+	};
 }
