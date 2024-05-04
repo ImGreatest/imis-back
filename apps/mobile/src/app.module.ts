bd8ab7411ed577278cb797713b3dbf2741049d3c
bulychev <den.bulychev@inbox.ru>
2024-05-04 18:43:19 +0300

    add user

--- apps/mobile/src/app.module.ts
+++ apps/mobile/src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './modules/user/user.module';
 import { PrismaModule } from 'libs/services/prisma/prisma.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from 'libs/services/auth/guard/auth.guard';
 import { AuthModule } from 'libs/services/auth/auth.module';
+import { RolesGuard } from 'libs/services/auth/guard/roles.guard';
+import { ScheduleModule } from '@nestjs/schedule';
+import { RatingControllerModule } from './modules/rating/rating.controller.module';
+import { UserModule } from './modules/user/user.module';
+
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule],
+  imports: [PrismaModule, AuthModule, UserModule,ScheduleModule.forRoot(), RatingControllerModule,
+    ],
   providers: [
     {
       provide: APP_GUARD,
--- apps/mobile/src/modules/user/user-controller.service.ts
+++ apps/mobile/src/modules/user/user-controller.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { IReqUpdateUser } from 'libs/domains/user/dto/req-dto/req-update-user.interface.dto';
+import { IResUser } from 'libs/domains/user/dto/res-dto/res-user.dto';
+import { UserService } from 'libs/domains/user/user.service';
 
 @Injectable()
-export class UserService {}
+export class UsersService {
+    constructor(private userServise: UserService) {}
+
+
+    updateUser(id: number, user: IReqUpdateUser): Promise<IResUser> {
+        return this.userServise.updateUser(id, user);
+      }
+
+}
