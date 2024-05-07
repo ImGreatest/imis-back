0b5d4cdc31afcca3803e98f925da433e7ecc6b76
Ermac <tulupov.valeriy@mail.ru>
2024-05-07 15:35:07 +0300

    f

--- apps/rating/src/modules/skill/skill.controller.module.ts
+++ apps/rating/src/modules/skill/skill.controller.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SkillModule } from 'libs/domains/skill/skill.module';
 import { SkillController } from './skill.controller';
 import { SkillControllerService } from './skill.controller.service';
+import { SkillTypeModule } from 'libs/domains/skill/skill-type.module';
 
 @Module({
-  imports: [SkillModule],
+  imports: [SkillModule, SkillTypeModule],
   providers: [SkillControllerService],
   controllers: [SkillController],
 })
