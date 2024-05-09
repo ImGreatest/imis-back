4c908e9091c866d93f9b7bc6efeb23ed70b6b93e
270rq <tulupov.valeriy@mail.ru>
2024-05-09 21:41:00 +0300

    fix

--- apps/mobile/src/app.module.ts
+++ apps/mobile/src/app.module.ts
@@ -3,16 +3,20 @@ import { PrismaModule } from 'libs/services/prisma/prisma.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from 'libs/services/auth/guard/auth.guard';
 import { AuthModule } from 'libs/services/auth/auth.module';
-import { RolesGuard } from 'libs/services/auth/guard/roles.guard';
 import { ScheduleModule } from '@nestjs/schedule';
 import { RatingControllerModule } from './modules/rating/rating.controller.module';
 import { UserModule } from 'libs/domains/user/user.module';
 import { UserControllerModule } from './modules/user/user-controller.module';
 
-
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule,UserControllerModule, ScheduleModule.forRoot(), RatingControllerModule,
-    ],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    UserControllerModule,
+    ScheduleModule.forRoot(),
+    RatingControllerModule,
+  ],
   providers: [
     {
       provide: APP_GUARD,
